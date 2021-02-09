f97a5a6683b1b6b31efd048a726aa81a5bf31683
Tarik Aboulhouda <tarikab92@yahoo.fr>
2021-02-09 14:03:10 +0100

    prise en compte de fichier / ajout de multer

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,30 +1,46 @@
 
 const Sauce = require("../models/Sauce");
+const fs = require("fs");
 
 exports.createThing = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce)
+    delete sauceObject._id;    
     const sauce = new Sauce({
-        ...req.body
+        ...sauceObject,
+        imageUrl : `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
     });
     sauce.save()
-        .then(() => res.status(201).json({ message : " Objet enregistré :"}))
+        .then(() => res.status(201).json({ message : " Sauce enregistré !"}))
         .catch(error => res.status(400).json({ error }));
 };
 
 exports.modifyThing = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }), { ...req.body, _id: req.params.id }
+    const sauceObject = req.file ?
+    {
+        ...JSON.parse(req.body.sauce),
+        imageUrl : `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+    } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }), { ...sauceObject, _id: req.params.id }
         .then(() => res.status(200).json({ message : " Objet modifié !"}))
         .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteThing = (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message : " Objet supprimé !"}))
-        .catch(error => res.status(400).json({ error }));
+    Sauce.findOne({_id : req.params.id })
+        .then( sauce => {
+            const filename = sauce.imageUrl.split("/images/")[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message : " Objet supprimé !"}))
+                    .catch(error => res.status(400).json({ error }));
+            })
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.getAllThings = (req, res, next) => {
     Sauce.find()
-        .then(sauces => res.status(200).json({ message : sauces}))
+        .then(sauces => res.status(200).json({ sauces }))
         .catch(error => res.status(400).json({ error }));
 };
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -3,18 +3,19 @@ const router = express.Router();
 
 const stuffCtrl = require("../controllers/stuff");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 
 // POST Capture et enregistre l'image, analyse la sauce en utilisant une chaîne de caractères et
 // l'enregistre dans la base de données, en définissant correctement son image URL. Remet les
 // sauces aimées et celles détestées à 0, et les sauces usersliked et celles usersdisliked aux tableaux vides.
-router.post("/", auth, stuffCtrl.createThing);
+router.post("/", auth, multer, stuffCtrl.createThing);
 
 // Met à jour la sauce avec l'identifiant fourni. Si une image est téléchargée, capturez-la
 // et mettez à jour l'image URL des sauces. Si aucun fichier n'est fourni, les détails de la
 // sauce figurent directement dans le corps de la demande (req.body.name, req.body.heat etc). Si
 // un fichier est fourni, la sauce avec chaîne est en req.body.sauce.
-router.put("/:id", auth, stuffCtrl.modifyThing);
+router.put("/:id", auth, multer,  stuffCtrl.modifyThing);
 
 // Supprime la sauce avec l'ID fourni
 router.delete("/:id", auth, stuffCtrl.deleteThing);
